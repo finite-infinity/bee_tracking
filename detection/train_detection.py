b3b3cf8d2e0201c4ebfe9b9c35d5f8fd42920f22
finite-infinity <50870852+finite-infinity@users.noreply.github.com>
2021-07-14 17:18:30 +0800

    Update train_detection.py

--- detection/train_detection.py
+++ detection/train_detection.py
@@ -195,7 +195,7 @@ class TrainModel:
         data = npz['data']
         t1 = time.time()
         train_steps = int(data.shape[0]*self.train_prop)
-        batch_data, last_relus = self._sample_offsets(data)
+        batch_data, last_relus = self._sample_offsets(data)  #直接用sample函数剪裁data，未用到IMG_DIR
 
         accuracy_t = np.zeros((6))
         for step in range(train_steps):
