9ac0ee3bc3aa9b575ce8e3dec27a4727443d466c
finite-infinity <50870852+finite-infinity@users.noreply.github.com>
2021-07-14 15:39:19 +0800

    Update dataset.py

--- detection/dataset.py
+++ detection/dataset.py
@@ -5,15 +5,15 @@ from utils.func import FR_D
 from utils import func, paths
 from scipy.stats import norm
 
-
-def ellipse_around_point(xc, yc, a, d, r1, r2):
-    ind = np.zeros((2, d, d), dtype=np.int)
+# 画椭圆
+def ellipse_around_point(xc, yc, a, d, r1, r2):  #d = FR_D
+    ind = np.zeros((2, d, d), dtype=np.int)  #坐标
     m = np.zeros((d, d), dtype=np.float32)
     for i in range(d):
         ind[0,:,i] = range(-yc, d-yc)
     for i in range(d):
-        ind[1,i,:] = range(-xc, d-xc)
-    rs1 = np.arange(r1, 0, -float(r1)/r2)
+        ind[1,i,:] = range(-xc, d-xc)  # 边长为1的（y,x）网格
+    rs1 = np.arange(r1, 0, -float(r1)/r2)  # r1用r2步减到0
     rs2 = np.arange(r2, 0, -1.0)
     s = math.sin(a)
     c = math.cos(a)
@@ -23,15 +23,15 @@ def ellipse_around_point(xc, yc, a, d, r1, r2):
         i1 = rs1[i]
         i2 = rs2[i]
         v = norm.pdf(float(len(rs1) - i) / len(rs1)) / pdf0
-    # rotated ellipse
+    # 经旋转的椭圆 m（((-yc,d-yc)*s+ic)/rs[i],(-xc,d-xc)*c+is）...)=v 
         m[((ind[0,:,:] * s + ind[1,:,:] * c)**2 / i1**2 + (ind[1,:,:] * s - ind[0,:,:] * c)**2 / i2**2) <= 1] = v
 
-    return m
+    return m   #shape（d,d） 是一个椭圆 
 
 
 def generate_segm_labels(img, pos, w=10, r1=7, r2=12):
     res = np.zeros((4, FR_D, FR_D), dtype=np.float32) # data,labels_segm, labels_angle, weight
-    res[0] = img
+    res[0] = img  #data
     res[2] = -1
 
     for i in range(pos.shape[0]):
@@ -44,13 +44,13 @@ def generate_segm_labels(img, pos, w=10, r1=7, r2=12):
             a = math.radians(float(a))
 
         if obj_class == 1:
-            m = ellipse_around_point(x, y, a, FR_D, r1, r2)
+            m = ellipse_around_point(x, y, a, FR_D, r1, r2)  
         else:
             m = ellipse_around_point(x, y, a, FR_D, r1, r1)
 
         mask = (m != 0)
-        res[1][mask] = obj_class
-        res[2][mask] = a / (2*math.pi)
+        res[1][mask] = obj_class  #类别号
+        res[2][mask] = a / (2*math.pi)  
         res[3][mask] = m[mask]
 
     res[3] = res[3]*(w - 1) + 1
@@ -68,5 +68,5 @@ def create_from_frames(frame_nbs, img_dir, pos_dir, out_dir=paths.DET_DATA_DIR):
         img = func.read_img(frame_nb, img_dir)
         pos = np.loadtxt(os.path.join(pos_dir, "%06d.txt" % frame_nb), delimiter=",", dtype=np.int)
         res[i] = generate_segm_labels(img, pos)
-    np.savez(os.path.join(out_dir, "%06d.npz" % fl_nb), data=res, det=pos)
+    np.savez(os.path.join(out_dir, "%06d.npz" % fl_nb), data=res, det=pos)  #存储res和pos 调用时 file=np.load('.npz')，file['data']=res
 
